2d9fea825ad452bb1159b570049c594fd0db0332
camillazicari <camyzicari01@gmail.com>
2024-11-13 16:49:43 +0100

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
 const insertTask = document.getElementById("insertTask");
 const btnInvio = document.getElementById("btnInvio");
 const lista = document.getElementById("lista");
+const frmToDoList = document.getElementById('toDoList');
 const listaTasks = [];
+let listItem;
 
 btnInvio.addEventListener("click", function (e) {
   e.preventDefault();
   if (!inputCheck()) return;
   addTasksArray();
   visualTasks();
+  frmToDoList.reset();
 });
 
 function inputCheck() {
@@ -26,8 +29,25 @@ function addTasksArray() {
 function visualTasks() {
   lista.innerHTML = "";
   for (let i = 0; i < listaTasks.length; i++) {
-    let listItem = document.createElement("li");
+    listItem = document.createElement("li");
     listItem.innerText = listaTasks[i];
+    let btnDelete = document.createElement('button');
+    btnDelete.setAttribute('type', 'button');
+    btnDelete.setAttribute('onclick', 'deleteTask()');
+    btnDelete.innerText = '🚮';
+    listItem.appendChild(btnDelete);
     lista.appendChild(listItem);
+
   }
 }
+
+function deleteTask(task) {
+    listaTasks.splice(task, 1);
+    visualTasks();
+}
+
+/*listItem.addEventListener('click', function {
+    let sbarrato = document.createElement('class');
+    sbarrato.setAttribute('name', 'sbarrato');
+    insertTask.appendChild(sbarrato);
+})*/
