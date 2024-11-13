65bdf74683b2d40b4a6b68a2bf5fe610f0df00a6
camillazicari <camyzicari01@gmail.com>
2024-11-13 15:34:09 +0100

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,23 @@
-const insertTask = document.getElementById('insertTask');
-const btnInvio = document.getElementById('btnIvio');
-const list = document.getElementById('list');
+const insertTask = document.getElementById("insertTask");
+const btnInvio = document.getElementById("btnInvio");
+const lista = document.getElementById("lista");
+const listaElements = [];
 
+btnInvio.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (!inputCheck()) return;
+  addTasks();
+});
 
+function inputCheck() {
+  if (insertTask.value === "") {
+    alert("campo non compilato");
+    return false;
+  } else {
+    return true;
+  }
+}
 
+function addTasks() {
+  listaElements.push(insertTask.value);
+}
